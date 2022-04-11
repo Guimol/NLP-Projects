ee332a6e09d755d379ecce223a8af9a82749ff03
Guilherme Oliveira <51828246+Guimol@users.noreply.github.com>
2022-04-11 18:03:37 -0300

    Delete fixacao1.py

--- SelfStudies/fixacao1.py
+++
@@ -1,8 +0,0 @@
-file = open('qbdata.txt', 'r')
-
-linhas = file.readlines()
-
-for linha in linhas:
-    linha = linha.split(' ')
-    if(linha[2] == 'QB,'):
-        print(f"{linha[0]} {linha[1]} teve valor {linha[len(linha) - 1]}")
