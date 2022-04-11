8dab18694b8c5957b12570c150f9206b79c7e161
Guilherme Oliveira <51828246+Guimol@users.noreply.github.com>
2022-04-11 18:02:12 -0300

    Delete ex10.py

--- SelfStudies/ex10.py
+++
@@ -1,7 +0,0 @@
-tupla = tuple(range(10))
-
-#tupla[3] = 10
-#Resultado: TypeError: 'tuple' object does not support item assignment
-
-print(f"Tupla: {tupla}")
-print(f"Indice do 5: {tupla.index(5)}")
